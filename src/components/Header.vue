71126c0802236a0a76ca83c807666a483c84e041
HayreddinTuzel <hayreddintuzel@gmail.com>
2019-05-12 18:51:45 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,13 +4,21 @@
       <a class="navbar-brand" href="#">Stock Trader</a>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <router-link tag="li" to="/stocks"  class="nav-item" active-class="active">
+          <router-link tag="li" to="/stocks" class="nav-item" active-class="active">
             <a class="nav-link">Stocks</a>
           </router-link>
           <router-link tag="li" to="/portfolio" class="nav-item" active-class="active">
             <a class="nav-link">Portfolio</a>
           </router-link>
         </ul>
+      </div>
+      <button class="btn btn-sm btn-secondary mr-2">End Day</button>
+      <div class="dropdown mr-2">
+        <button class="btn btn-sm dropdown-toggle" data-toggle="dropdown">Save & Load</button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Save</a>
+            <a class="dropdown-item" href="#">Load</a>
+          </div>
       </div>
     </nav>
   </div>
