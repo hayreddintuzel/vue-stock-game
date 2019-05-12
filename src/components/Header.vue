7b6cefb5eaaca36f868ff94ce8313195d4b9fa63
HayreddinTuzel <hayreddintuzel@gmail.com>
2019-05-12 17:46:38 +0300

    project start

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Stock Trader</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <router-link tag="li" to="/stocks"  class="nav-item" active-class="active">
+            <a class="nav-link">Stocks</a>
+          </router-link>
+          <router-link tag="li" to="/portfolio" class="nav-item" active-class="active">
+            <a class="nav-link">Portfolio</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+
+</script>
